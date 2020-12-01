68b45b80a92a73300702eaa5cd6faba9f13ff33a
abdi2148 <abdiqafar.abas@gmail.com>
2020-12-02 00:35:03 +0100

    models added

---
+++ Ace-Autos-App-Angular/src/app/shared/models/car.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  id: number;
+  manufacturer: string;
+  year: number;
+  mileage: string;
+  fuel: string;
+  color: string;
+  model: string;
+  price: number;
+  type: string;
+  description;
+}
