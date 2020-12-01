0575d70cb606ec46fd012565175a6a916b8cb962
Ajay0411943 <44640758+Ajay0411943@users.noreply.github.com>
2020-12-01 16:53:47 +0100

    html and css fixed and pages created

--- Ace-Autos-App-Angular/src/app/app-routing.module.ts
+++ Ace-Autos-App-Angular/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import {CarsComponent} from "./cars/cars.component";
 import {ContactComponent} from "./contact/contact.component";
 import {LoginComponent} from "./login/login.component";
 import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: 'cars', component: CarsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: "home", pathMatch: 'full' }
 ];
 
 
