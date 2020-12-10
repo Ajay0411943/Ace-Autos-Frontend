5f799c3f4e62d2090f7e00aa69e34e90dada1bb3
abdi2148 <abdiqafar.abas@gmail.com>
2020-12-10 19:37:39 +0100

    Revert "Authentication"
    This reverts commit 46258d4398e03229f67e699bd8fe86f624c4b9b5.

--- Ace-Autos-App-Angular/src/app/login/login.component.ts
+++ Ace-Autos-App-Angular/src/app/login/login.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from '../shared/services/authentication.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -10,41 +6,10 @@ import {environment} from '../../environments/environment';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  submitted = false;
-  loading = false;
 
-   httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      Authorization: 'my-auth-token'
-    })
-  };
-  constructor(private authenticationService: AuthenticationService,
-              private formBuilder: FormBuilder,
-              private http: HttpClient) { }
+  constructor() { }
 
-  ngOnInit(): any {
-    //  Initialize the form group
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
-    // reset login status
-    this.authenticationService.logout();
+  ngOnInit(): void {
   }
 
-  get username() { return this.loginForm.get('username'); }
-  get password() { return this.loginForm.get('password'); }
-
-  onSubmit(): any {
-    this.authenticationService.login(this.username.value, this.password.value);
-  }
-
-  getList(): any{
-    this.http.get<any>(environment.apiURL + 'car', this.httpOptions).subscribe(data => {
-      console.table(data);
-    });
-  }
 }
