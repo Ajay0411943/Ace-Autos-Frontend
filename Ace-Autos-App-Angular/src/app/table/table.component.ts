a84d393d466d5174a0990da821a39538ede1f65a
abdi2148 <abdiqafar.abas@gmail.com>
2020-12-18 10:56:23 +0100

    Table

--- Ace-Autos-App-Angular/src/app/table/table.component.ts
+++ Ace-Autos-App-Angular/src/app/table/table.component.ts
@@ -1,34 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 
-export class TableComponent  {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-}
 
+export class TableComponent {
+  title = 'Car Table';
+  searchText;
+  cars = [
+    {id: 1, manufacturer: 'Audi', model: 'R8', type: 'Sports', year: 2015, fuel: 'Gas', mileage: 1000, color: 'Red', price: 100000},
+    {id: 2, manufacturer: 'BMW', model: 'M8', type: 'Sports', year: 1, fuel: 'Gas', mileage: 1000, color: 'Black', price: 100000},
+    {id: 3, manufacturer: 'Mercedes', model: 'S class', type: 'Luxury', year: 4, fuel: 'Gas', mileage: 1000, color: 'White', price: 100000},
+    {id: 4, manufacturer: 'Porsche', model: 'Camaro', type: 'Sports', year: 1, fuel: 'Gas', mileage: 1000, color: 'Yellow', price: 100000},
+    {id: 5, manufacturer: 'Toyota', model: 'Supra', type: 'Sports', year: 6, fuel: 'Gas', mileage: 1000, color: 'Blue', price: 100000},
+    {id: 6, manufacturer: 'Volkswagen', model: 'Golf', type: 'Sports', year: 9, fuel: 'Gas', mileage: 1000, color: 'Green', price: 100000},
+    {id: 7, manufacturer: 'Honda', model: 'Civic', type: 'Sports', year: 15, fuel: 'Gas', mileage: 1000, color: 'Brown', price: 100000},
+    {id: 8, manufacturer: 'Suzuki', model: 'Swift', type: 'Sports', year: 6, fuel: 'Gas', mileage: 1000, color: 'Purple', price: 100000},
+    {id: 9, manufacturer: 'Nissan', model: 'Skyline', type: 'Sports', year: 10, fuel: 'Gas', mileage: 1000, color: 'Silver', price: 100000},
+  ];
+}
