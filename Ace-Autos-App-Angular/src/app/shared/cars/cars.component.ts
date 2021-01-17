06922f67a6caf5ac6a7b74a5d04c4d5bc624c79b
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-01-17 12:57:13 +0100

    Comments/Notes

--- Ace-Autos-App-Angular/src/app/shared/cars/cars.component.ts
+++ Ace-Autos-App-Angular/src/app/shared/cars/cars.component.ts
@@ -9,23 +9,31 @@ import {CarsService} from '../services/cars/cars.service';
 })
 export class CarsComponent implements OnInit {
 
-  cars: Car[];
+  cars: Car[]; /* Type safety */
   constructor(private carService: CarsService) { }
   ngOnInit(): void {
     // this.cars = this.carService.getCars();
     this.refresh();
   }
 
+  /**
+   * In here, for deleting a car, the service is called.
+   */
   delete(id: number): any {
     console.log(id);
     this.carService.deleteCar(id);
-    // this.cars = this.carService.getCars();
     this.refresh();
     location.reload();
+    /**
+     * Last two lines of code are put in place to retrieve list of cars, after deletion, and reloading the view.
+     */
+
   }
 
+  /**
+   * Subscribe is used to obtain or generate values.
+   */
   refresh(): any {
-    console.log('Refreshing');
     this.carService.getCars()
       .subscribe(listOfCars => {
         this.cars = listOfCars;
